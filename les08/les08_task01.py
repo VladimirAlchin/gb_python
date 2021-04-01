cff183d72d93ca28cae9ca676ade2882c8d08d81
Vladimir <79061188133@ya.ru>
2021-04-01 22:49:49 +0300

    Полное решение, без оптимизации

--- les08/les08_task01.py
+++ les08/les08_task01.py
@@ -100,24 +100,37 @@ class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-        self.move = list(range(1, 91))
+
 
     def move(self):
-        while len(self.move) > 75:
+        self.move = list(range(1, 91))
+        while len(self.move) > 0:
             print(self.p1)
             print(self.p2)
             del_number = random.randint(0, len(self.move) - 1)
-            user_ans = input(f'Выпало число: {self.move[del_number]}\nЗачеркнуть цифру? y/n ')
+            user_ans = input(f'Выпало число: {self.move[del_number]} (Осталось {len(self.move)-1})\nЗачеркнуть цифру? y/n ')
             for i in range(0, 3):
                 try:
                     del_player_number = self.p1[i].index(self.move[del_number])
                     self.p1[i][del_player_number] = '--'
-                    # self.fine += 1
+                    self.p1.fine += 1
                     break
                 except ValueError:
                     del_player_number = -1
-            del self.move[del_player_number]
-            # print(self.fine)
+                try:
+                    del_pc_number = self.p2[i].index(self.move[del_number])
+                    self.p2[i][del_pc_number] = '--'
+                    self.p2.fine += 1
+                    break
+                except ValueError:
+                    pass
+            del self.move[del_number]
+            if self.p1.fine == 15:
+                print('Игрок победил')
+                exit()
+            if self.p2.fine == 15:
+                print('ПК победил')
+                exit()
             if (user_ans == 'y' and del_player_number == -1) or (user_ans == 'n' and del_player_number > -1):
                 print('Вы проиграли, приходите еще!')
                 exit()
